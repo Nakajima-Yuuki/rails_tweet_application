ff61e5c2b8fc5e380c4a14e6e870a7d07c61ba0b
Yuuki <ukmmm0523@gmail.com>
2021-07-09 15:34:45 +0900

    共通ページ更新

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
   def index
 
-    @posts = [
-      
-      "今日からでRailsの勉強するよー！",
-      "投稿一覧ページ作成中！"
-    ]
+    @posts = Post.all
+  
+  
   end
 end
