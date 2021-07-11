dd30280442e36dc6795c82da6543b33c81e63a67
Yuuki <ukmmm0523@gmail.com>
2021-07-11 19:32:13 +0900

    実行メッセージ表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,11 +8,13 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
   end
   
   def create
     @post = Post.new(content: params[:content])
     if @post.save
+      flash[:notice]="投稿を作成しました！"
       redirect_to("/posts/index")
     else
       render("posts/new")
@@ -37,6 +39,7 @@ end
   def destroy
     @post = Post.find_by(id: params[:id])
     @post.destroy
+    flash[:notice]="投稿を削除しました！"
     redirect_to("/posts/index")
   
   end
